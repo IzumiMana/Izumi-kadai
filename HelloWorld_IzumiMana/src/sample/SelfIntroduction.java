8845e63446be592c951ed03a21d2a33be4bf10bd
mana <mana@izumiaiisanoair>
2025-07-22 22:50:56 +0900

    Java3 Self introduction

---
+++ HelloWorld_IzumiMana/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 夢
+		String dream = "プログラマーになること";
+		// 年齢
+		int age = 20;
+		// 趣味
+		String hobby = "音楽鑑賞";
+
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		
+		System.out.println("私の夢は" + dream + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");	
+	}
+	 
+}
